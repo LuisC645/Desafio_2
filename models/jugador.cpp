9f68efce69c13e32aff893e7ff41672b6d0ae7c7
LuisC645 <luisandres645@gmail.com>
2026-04-22 19:26:08 -0500

    Creacion clase jugadores

---
+++ models/jugador.cpp
@@ -0,0 +1,73 @@
+#include "jugador.h"
+
+// Constructor
+jugador::jugador(string nombre, string posicion, unsigned short dorsal):
+    nombre(nombre), posicion(posicion), dorsal(dorsal),
+    golesPartido(0), amarillasPartido(0), rojasPartido(0),
+    faltasPartido(0), minutosPartido(0), golesTotales(0), rojasTotales(0) {}
+
+
+// Getters
+string jugador::getName() const{
+    return nombre;
+}
+string jugador::getPos() const{
+    return posicion;
+}
+unsigned short jugador::getDor() const {
+    return dorsal;
+}
+
+// Getters Partido
+unsigned short jugador::getGolesPartido() const {
+    return golesPartido;
+}
+unsigned short jugador::getAmarillasPartido() const {
+    return amarillasPartido;
+}
+unsigned short jugador::getRojasPartido() const {
+    return rojasPartido;
+}
+unsigned short jugador::getFaltasPartido() const {
+    return faltasPartido;
+}
+unsigned int jugador::getMinutosPartido() const {
+    return minutosPartido;
+}
+unsigned short jugador::getGolesTotales() const {
+    return golesTotales;
+}
+
+// Funciones
+void jugador::registerGol(){
+    golesPartido ++;
+    golesTotales ++;
+}
+void jugador::registerAmarilla() {
+    amarillasPartido++;
+    if (amarillasPartido >= 2) {
+        registerRoja();
+    }
+}
+void jugador::registerRoja() {
+    rojasPartido = 1;
+    rojasTotales++;
+}
+void jugador::registerFalta() {
+    faltasPartido++;
+}
+void jugador::setMinPartido(unsigned short min) {
+    minutosPartido = min;
+}
+
+// Reiniciar partido
+void jugador::restartStatsPartido() {
+    golesPartido = 0;
+    amarillasPartido = 0;
+    rojasPartido = 0;
+    faltasPartido = 0;
+    minutosPartido = 0;
+}
+
+
+
---
+++ models/jugador.h
@@ -0,0 +1,54 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include <string>
+
+using namespace std;
+
+class jugador
+{
+private:
+
+    string nombre;
+    string posicion;
+    unsigned short dorsal;
+
+    unsigned short golesPartido;
+    unsigned short amarillasPartido;
+    unsigned short rojasPartido;
+    unsigned short faltasPartido;
+    unsigned short minutosPartido;
+    unsigned short golesTotales;
+    unsigned short rojasTotales;
+
+public:
+
+    // Constructor
+    jugador(string nombre, string posicion, unsigned short dorsal);
+
+    // Getters
+    string getName() const;
+    string getPos() const;
+    unsigned short getDor() const;
+
+    unsigned short getGolesPartido() const;
+    unsigned short getAmarillasPartido() const;
+    unsigned short getRojasPartido() const;
+    unsigned short getFaltasPartido() const;
+    unsigned int getMinutosPartido() const;
+    unsigned short getRojasTotales() const;
+    unsigned short getGolesTotales() const;
+
+    // Funciones
+    void registerGol();
+    void registerAmarilla();
+    void registerRoja();
+    void registerFalta();
+    void setMinPartido(unsigned short min);
+
+    // Reiniciar partido
+    void restartStatsPartido();
+
+};
+
+#endif // JUGADOR_H
